c93e6b61c94fc224f15e0c59c25e17229f7e65a6
Thomaz Leite <thmzlt@gmail.com>
2011-07-16 14:51:44 -0400

    Implement email receipt

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,2 +1,11 @@
 class EmailsController < ApplicationController
+  
+  skip_before_filter :verify_authenticity_token, :only => :create
+  
+  def create
+    address = params["address"]
+    @email = Email.new(:address => address)
+    @email.save
+    render :nothing => true
+  end
 end
