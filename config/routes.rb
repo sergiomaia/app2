8e3e1cb0c18c172d79a4c8e854f9bb14da1f7fe2
sergiomaia <sergiomaia@gmail.com>
2011-07-16 17:10:43 -0300

    add routes to sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ App2::Application.routes.draw do
 
   resources :emails
   resources :users
+  resources :sessions
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
